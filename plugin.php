97dcb871460c66ff6a4dd38dbe7f1da1ad88d9f7
JuanAguilarG <nimsul002@gmail.com>
2023-10-02 13:10:05 +0200

    2023.10.02 Working in Ilias 8

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srgg";
-$version = "1.2.4";
+$version = "1.3.0";
 $ilias_min_version = "8.0";
 $ilias_max_version = "8.999";
 $responsible = "fluxlabs ag";
