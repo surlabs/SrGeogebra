73d9a5f2396d3ac563bb80854cc47ddfb04861d2
Saúl Díaz <62429591+Saaweel@users.noreply.github.com>
2024-09-10 13:34:03 +0200

    Version 1.3.4

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "srgg";
-$version = "1.3.3";
+$version = "1.3.4";
 $ilias_min_version = "8.0";
 $ilias_max_version = "8.999";
 $responsible = "fluxlabs ag";
